f27f816c7107ea782e2b8ea865211ba2c1efe7e5
Matt Chiswell <24735648+MattChiswell@users.noreply.github.com>
2026-03-20 23:07:29 +0000

    refactor: ensure ANSI codes are stored correctly

--- lib/core/logging.sh
+++ lib/core/logging.sh
@@ -6,23 +6,23 @@
 
 function log::setup_term_colours() {
   if [[ -t 1 ]]; then
-    LOG_COLOUR_INFO="\033[1;37m"  # white bold
-    LOG_COLOUR_WARN="\033[1;33m"  # yellow bold
-    LOG_COLOUR_DEBUG="\033[1;34m" # blue bold
-    LOG_COLOUR_ERROR="\033[1;31m" # red bold
-    LOG_COLOUR_GREEN="\033[1;32m" # green bold
-    LOG_COLOUR_CYAN="\033[1;36m"  # cyan bold
-    LOG_COLOUR_BLACK="\033[0;90m" # black high-intensity [looks like light gray]
-    LOG_COLOUR_RESET="\033[0m"
+    export LOG_COLOUR_INFO=$'\033[1;37m'  # white bold
+    export LOG_COLOUR_WARN=$'\033[1;33m'  # yellow bold
+    export LOG_COLOUR_DEBUG=$'\033[1;34m' # blue bold
+    export LOG_COLOUR_ERROR=$'\033[1;31m' # red bold
+    export LOG_COLOUR_GREEN=$'\033[1;32m' # green bold
+    export LOG_COLOUR_CYAN=$'\033[1;36m'  # cyan bold
+    export LOG_COLOUR_BLACK=$'\033[0;90m' # black high-intensity [looks like light gray]
+    export LOG_COLOUR_RESET=$'\033[0m'
   else
-    LOG_COLOUR_INFO=""
-    LOG_COLOUR_WARN=""
-    LOG_COLOUR_DEBUG=""
-    LOG_COLOUR_ERROR=""
-    LOG_COLOUR_GREEN=""
-    LOG_COLOUR_CYAN=""
-    LOG_COLOUR_BLACK=""
-    LOG_COLOUR_RESET=""
+    export LOG_COLOUR_INFO=""
+    export LOG_COLOUR_WARN=""
+    export LOG_COLOUR_DEBUG=""
+    export LOG_COLOUR_ERROR=""
+    export LOG_COLOUR_GREEN=""
+    export LOG_COLOUR_CYAN=""
+    export LOG_COLOUR_BLACK=""
+    export LOG_COLOUR_RESET=""
   fi
 }
 
